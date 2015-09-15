c1a8665f89206049f5b260389c104712e8ab1801
xeon13 <xeonbaseball13@gmail.com>
2015-09-15 13:35:29 -0600

    Stuff and Things

---
+++ Assets/Scripts/Creep.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Creep : MonoBehaviour {
+	private int health;
+	private int speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public int getHealth() {
+		return this.health;
+	}
+
+	public void setHealth(int health) {
+		this.health = health;
+	}
+
+	public int getSpeed() {
+		return this.speed;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+}
---
+++ Assets/Scripts/Peasant.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Peasant : Creep {
+	private int initialHealth = 20;
+	private int initialSpeed = 2;
+
+	// Use this for initialization
+	void Start () {
+		this.setHealth(initialHealth);
+		this.setSpeed(initialSpeed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
